ee001ae62299bb3a4cda41b2900c9f396bbcce73
Bipro23 <biprodeep2301@gmail.com>
2024-05-19 18:03:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/919864810337?text=Hi%20let%27s%20waste%20more%20time%20💕💕';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert("That's fine, it was nice talking with you.💕");
+});
